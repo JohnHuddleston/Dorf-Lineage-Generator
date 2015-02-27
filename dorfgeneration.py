135d6f766fef56cc4734743a119efb7c055940fb
John Huddleston <jhuddleston5646@gmail.com>
2015-02-26 21:11:25 -0500

    Restructured arrays and use thereof

--- dorfgeneration.py
+++ dorfgeneration.py
@@ -1,14 +1,14 @@
 import random
 
 #dictionaries for names
-maleNameDict = {'first':["Ar", "Dar", "Ur", "Sur", "Lad", "Saf", "Raf", "Der", "Bo", "Gam"], 
-'last':["ist", "ust", "all", "rall", "sohn", "tist", "tol", "dol", "far", "var", "mar", "fur", "ril"], }
+maleFirst = ["Ar", "Dar", "Ur", "Sur", "Lad", "Saf", "Raf", "Der", "Bo", "Gam"]
+maleLast = ["ist", "ust", "all", "rall", "sohn", "tist", "tol", "dol", "far", "var", "mar", "fur", "ril"]
 
-femaleNameDict = {'first':["D", "Sir", "Sar", "Laur", "Dahl", "Mish", "Sish", "Sash", "Tam", "Cam", "Da"], 
-'last':["a", "ah", "ka", "da", "ta", "is", "isha", "ishta", "ora", "era"] }
+femaleFirst = ["D", "Sir", "Sar", "Laur", "Dahl", "Mish", "Sish", "Sash", "Tam", "Cam", "Da"]
+femaleLast = ["a", "ah", "ka", "da", "ta", "is", "isha", "ishta", "ora", "era"] 
 
-familyNameDict = {'first':["Iron", "Gold", "Red", "Dark", "Stone", "Anvil", "Hammer"], 
-'last':["son", "hammer", "forge", "reach", "crown", "kil", "rett", "mort", "sort"] }
+familyFirst = ["Iron", "Gold", "Red", "Dark", "Stone", "Anvil", "Hammer"]
+familyLast = ["son", "hammer", "forge", "reach", "crown", "kil", "rett", "mort", "sort"] 
 
 #generic dwarf class
 class Dwarf(object):
@@ -16,18 +16,18 @@ class Dwarf(object):
 		self.family = family
 		self.age = age
 	age = random.uniform(40, 300)
-	family = familyNameDict['first'[int(random.uniform(0, len(familyNameDict['first']) + 1))]] + familyNameDict['last'[int(random.uniform(0, len(familyNameDict['last']) + 1))]]
+	family = random.choice(familyFirst) + random.choice(familyLast)
 
 #gender-specific classes	
 class Male(Dwarf):
 	def __init__(self, name):
 		self.name = name
-	name = maleNameDict['first'[int(random.uniform(0, len(maleNameDict['first']) + 1))]] + maleNameDict['last'[int(random.uniform(0, len(maleNameDict['last']) + 1))]]
+	name = random.choice(maleFirst) + random.choice(maleLast)
 
 class Female(Dwarf):
 	def __init__(self, name):
 		self.name = name
-	name = femaleNameDict['first'[int(random.uniform(0, len(femaleNameDict['first']) + 1))]] + femaleNameDict['last'[int(random.uniform(0, len(femaleNameDict['last']) + 1))]]
+	name = random.choice(femaleFirst) + random.choice(femaleLast)
 
 newDwarf = Male()
 print newDwarf.age + newDwarf.name + newDwarf.family
