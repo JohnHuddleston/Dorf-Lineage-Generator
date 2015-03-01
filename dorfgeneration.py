4fe05355b14997244efc56665dbcf06f43da9c0e
John Huddleston <jhuddleston5646@gmail.com>
2015-03-01 16:22:25 -0500

    Expanding name libraries

--- dorfgeneration.py
+++ dorfgeneration.py
@@ -1,14 +1,14 @@
 import random
 
 #dictionaries for names
-maleFirst = ["Ar", "Dar", "Ur", "Sur", "Lad", "Saf", "Raf", "Der", "Bo", "Gam"]
-maleLast = ["ist", "ust", "all", "rall", "sohn", "tist", "tol", "dol", "far", "var", "mar", "fur", "ril"]
+maleFirst = ["Ar", "Dar", "Ur", "Sur", "Lad", "Saf", "Raf", "Der", "Bo", "Gam", "Bom", "Da", "Dar", "Dan", "Khi", "Khim", "Lo", "Bel", "Rei", "Thr", "Thra", "Yur"]
+maleLast = ["ist", "ust", "all", "rall", "sohn", "tist", "tol", "dol", "far", "var", "mar", "fur", "ril", "ghâl", "in", "fin", "fir", "bur", "bir", "gar"]
 
-femaleFirst = ["D", "Sir", "Sar", "Laur", "Dahl", "Mish", "Sish", "Sash", "Tam", "Cam", "Da"]
-femaleLast = ["a", "ah", "ka", "da", "ta", "is", "isha", "ishta", "ora", "era"] 
+femaleFirst = ["D", "Sir", "Sar", "Laur", "Dahl", "Mish", "Sish", "Sash", "Tam", "Cam", "Da", "Lo", "Dar", "Dash", "Hish"]
+femaleLast = ["a", "ah", "ka", "da", "ta", "is", "isha", "ishta", "ora", "era", "hilda", "theow", "gal"] 
 
-familyFirst = ["Iron", "Gold", "Red", "Dark", "Stone", "Anvil", "Hammer"]
-familyLast = ["son", "hammer", "forge", "reach", "crown", "kil", "rett", "mort", "sort"] 
+familyFirst = ["Iron", "Gold", "Red", "Dark", "Stone", "Anvil", "Hammer", "Wolf", "Barrel", "Axe", "Ash", "Amber", "Righteous", "Twilight", "White", "Wind", "Holy", "Glander", "Earth", "Emerald", "Lantern", "Cask", "Cavern"]
+familyLast = ["son", "hammer", "forge", "reach", "crown", "kil", "rett", "mort", "sort", "flask", "ale", "arm", "belt", "bringer", "sword", "kindler", "bane", "hood", "mace", "mover", "smith", "speaker"] 
 
 #gender-specific classes	
 class Male(object):
